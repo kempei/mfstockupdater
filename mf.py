166031af9f6232a5917166ac21d10f64e99c9d64
Igarashi Family <igarashi@IgarashinoMac-mini.local>
2023-08-12 21:05:01 +0900

    delete unused imports

--- mf.py
+++ mf.py
@@ -1,10 +1,7 @@
 from logzero import logger
 import logzero
 
-import selenium
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
